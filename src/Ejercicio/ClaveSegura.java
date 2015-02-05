98656532683eaf58d96f57f9a3870b438f54ec5a
Luis Usero Reyes <luis@luis-VPCEH2J1E>
2015-02-05 19:16:44 +0100

    intentor realizar el ejercicio 2

--- src/Ejercicio/ClaveSegura.java
+++ src/Ejercicio/ClaveSegura.java
@@ -1,51 +1,56 @@
 package Ejercicio;
 
 public class ClaveSegura {
-	public static boolean esClaveSegura(String s){
+	private String clave;
+	
+	public ClaveSegura(String clave) {
+		this.clave = clave;
+	}
+	public boolean esClaveSegura(){
 		//boolean aux = false;
-		return minimoLongitudOcho(s) && letraMayuscula(s) && letraMinuscula(s) 
-				&& tieneDigito(s) && tieneDigitoOLetra(s);
+		return minimoLongitudOcho() && letraMayuscula() && letraMinuscula() 
+				&& tieneDigito() && tieneDigitoOLetra();
 		
 		//return aux;	
 	}
-	private static boolean minimoLongitudOcho(String s){
-		return s.length() >= 7 ;
+	private boolean minimoLongitudOcho(){
+		return clave.length() >= 7 ;
 	}
 	
-	private static boolean letraMayuscula(String s){
+	private boolean letraMayuscula(){
 		boolean aux=false;
-		for (int i = 0; i < s.length(); i++) {
-		if (Character.isUpperCase(s.charAt(i))){
+		for (int i = 0; i < clave.length(); i++) {
+		if (Character.isUpperCase(clave.charAt(i))){
 			aux=true;
 			break;
 		}
 	}
 		return aux;	
 }
-	private static boolean letraMinuscula(String s){
+	private boolean letraMinuscula(){
 		boolean aux=false;
-		for (int i = 0; i < s.length(); i++) {
-		if (Character.isLowerCase(s.charAt(i))){
+		for (int i = 0; i < clave.length(); i++) {
+		if (Character.isLowerCase(clave.charAt(i))){
 			aux=true;
 			break;
 		}
 	}
 		return aux;
 }
-	private static boolean tieneDigito(String s){
+	private boolean tieneDigito(){
 		boolean aux=false;
-		for (int i = 0; i < s.length(); i++) {
-		if (Character.isDigit(s.charAt(i))){
+		for (int i = 0; i < clave.length(); i++) {
+		if (Character.isDigit(clave.charAt(i))){
 			aux=true;
 			break;
 		}
 	}
 		return aux;
 }
-	private static boolean tieneDigitoOLetra(String s){
+	private boolean tieneDigitoOLetra(){
 		boolean aux=false;
-		for (int i = 0; i < s.length(); i++) {
-		if (!Character.isLetterOrDigit(s.charAt(i))){
+		for (int i = 0; i < clave.length(); i++) {
+		if (!Character.isLetterOrDigit(clave.charAt(i))){
 			aux=true;
 			break;
 		}
--- src/Ejercicio/TestClaveSegura.java
+++ src/Ejercicio/TestClaveSegura.java
@@ -7,10 +7,15 @@ public class TestClaveSegura {
 		// TODO Auto-generated method stub
 		final String FUENTE_CARACTERES = "aAbBcCdDeEfFgGhHiIjJkKlLm-MnN~n~NoOpPqQrRsStTuUvVwWxXyYzZ0123456789>?()=@.:,;!¡&{}0123456789>?()=@.:,;!¡&{}";
 		
-		int longitud  = Math.round(0)*FUENTE_CARACTERES.length();
+		int longitud  = (int) (Math.random()*FUENTE_CARACTERES.length());
 		StringBuilder sBuilder = new StringBuilder();
 		for (int i = 0; i < longitud; i++) {
-			int posicion 
+			String clave = "";
+			int posicion = (int) (Math.random()*longitud);
+			clave+=FUENTE_CARACTERES.charAt(posicion);
+			String.valueOf(sBuilder.append(i)).toString();
+			System.out.println(clave.replaceAll("[\n\r]",""));
+			
 			
 		}
 		
